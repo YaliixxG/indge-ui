17625a364a2b7192e59953318ae93625d0cb538d
YaliixxG <529008421@qq.com>
2020-12-16 11:06:33 +0800

    perf: 添加统一注册组件 JS

--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/reset.css'
 import './assets/font/iconfont.css'
-import Button from './components/button'
-import Dialog from './components/dialog'
-import Input from './components/input'
-import Switch from './components/switch'
+import './components'
 
-Vue.component(Button.name, Button)
-Vue.component(Dialog.name, Dialog)
-Vue.component(Input.name, Input)
-Vue.component(Switch.name, Switch)
 Vue.config.productionTip = false
 
 /* eslint-disable */
