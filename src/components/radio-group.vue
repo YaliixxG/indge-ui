4463c907d6cf931dcdf9ab2240daba0482212dee
YaliixxG <529008421@qq.com>
2020-12-18 11:25:57 +0800

    perf: 去除冗余代码

--- src/components/radio-group.vue
+++ src/components/radio-group.vue
@@ -12,9 +12,6 @@ export default {
             default: ''
         }
     },
-    mounted() {
-        console.log('this', this.value)
-    },
     provide() {
         return {
             RadioGroup: this
