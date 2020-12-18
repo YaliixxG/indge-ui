fc3a4f1e9a2b416798287cfe4d89cd51b3f458fd
YaliixxG <529008421@qq.com>
2020-12-18 16:35:03 +0800

    feat: 添加 CheckboxGroup 组件

---
+++ src/components/checkbox-group.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="i-checkbox-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "ICheckboxGroup",
+    props: {
+        value: {
+            type: Array
+        }
+    },
+    provide() {
+        return {
+            CheckboxGroup: this
+        };
+    }
+};
+</script>
+<style lang="scss">
+</style>
+
